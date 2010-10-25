aa933e3db4ae5f6159e1bae11e58bf062fd58521
Jimmy Theis <jimmy@jetheis.com>
2010-10-25 03:47:19 -0400

    Add initial method stubs and test code
    These will deal in changeset generation.
    
    Signed-off-by: Jimmy Theis <jimmy@jetheis.com>

---
+++ client/policypad.js
@@ -0,0 +1,26 @@
+/**
+* Copyright 2010 PolicyPad 
+* 
+* Licensed under the Apache License, Version 2.0 (the "License"); 
+* you may not use this file except in compliance with the License. 
+* You may obtain a copy of the License at 
+*
+*     http://www.apache.org/licenses/LICENSE-2.0
+*
+* Unless required by applicable law or agreed to in writing, software 
+* distributed under the License is distributed on an "AS IS" BASIS, 
+* WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. 
+* See the License for the specific language governing permissions and 
+* limitations under the License.
+*/
+
+
+/**
+ * Compares oldText and newText, generating a changeset of the differences.
+ * @param {String} oldText The original editor text.
+ * @param {String} newText The editor text after changes have been made.
+ * @return A changeset representing the differences between oldText and newText.
+ */
+function generateChangeset(oldText, newText) {
+    return 'Z:';
+}
---
+++ client/test/changeset_tests.js
@@ -0,0 +1,115 @@
+/**
+ * Copyright 2010 PolicyPad
+ *
+ *  Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+
+/**
+ * These test deal with generating changesets based in user 
+ * insertions in the editor.
+ */
+module("Insertion Changeset Generation");
+
+/**
+ * Old: foobar
+ * New: foobarbaz
+ *
+ * Expected: Z:6>3=6+3$baz
+ */
+test('simple single line append', function() {
+    equals(generateChangeset('foobar', 'foobarbaz'), 'Z:6>3=6+3$baz');
+});
+
+
+/**
+ * Old: fo
+ * New: foo
+ *
+ * Expected: Z:2>1=2+1$o
+ */
+test('single character append', function() {
+    equals(generateChangeset('fo', 'foo'), 'Z:2>1=2+1$o');
+});
+
+
+/**
+ * Old: foobaz
+ * New: foobarbaz
+ *
+ * Expected: Z:6>3=3+3$bar
+ */
+test('simple single line insert', function() {
+    equals(generateChangeset('foobaz', 'foobarbaz'), 'Z:6>3=3+3$bar');
+});
+
+
+/**
+ * Old: foo\n
+ * New: foo\nbar
+ * 
+ * Expected: Z:4>3|1=4+3$bar
+ */
+test('simple multi line append', function() {
+    equals(generateChangeset('foo\n', 'foo\nbar'), 'Z:4>3|1=4+3$bar');
+});
+
+
+/**
+ * Old: foo\nbar
+ * New: foo\nbar\nbaz
+ *
+ * Expected: Z:7>4|1=4=3+4$\nbaz
+ */
+test('simple multi line append without original newline', function() {
+    equals(generateChangeset('foo\nbar', 'foo\nbar\nbaz'), 'Z:7>4|1=4=3+4$\nbaz');
+});
+
+
+/**
+ * Old: foo\nbaz
+ * New: foo\nbar\nbaz
+ *
+ * Expected: Z:7>4|1=4+4$bar\n
+ */
+test('simple multi line insert', function() {
+    equals(generateChangeset('foo\nbaz', 'foo\nbar\nbaz'), 'Z:7>4|1=4+4$bar\n');
+});
+
+
+/**
+ * These tests deal with generating changesets based on deletions
+ * from within the editor.
+ */
+module('Deletion Changeset Generation');
+
+/**
+ * Old: fooo
+ * New: foo
+ *
+ * Expected: Z:4<1=3-1
+ */
+test('simple single character delete', function() {
+    equals(generateChangeset('fooo', 'foo'), 'Z:4<1=3-1');
+});
+
+
+/**
+ * Old: foobarbaz
+ * New: foobaz
+ *
+ * Expected: Z:6<3=3-3
+ */
+test('simple single line characters delete', function() {
+    equals(generateChangeset('foobarbaz', 'foobaz'), 'Z:6<3=3-3');
+});
