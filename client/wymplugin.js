5daca73d8de331dc2b5fe69c5850c202bc954c75
Kevin Wells <wellska1@rose-hulman.edu>
2010-11-01 00:10:20 -0400

    Add javascript library tests and wymplugin
    Signed-off-by: Kevin Wells <wellska1@rose-hulman.edu>

---
+++ client/wymplugin.js
@@ -0,0 +1,31 @@
+
+
+/*
+insert(string) -> void - insert text at current cursor position
+html([string]) -> [string] - get/set html content
+xhtml() -> string - get xhtml content
+status(string) => void - display text in editor's status bar
+
+*/
+
+//Extend WYMeditor
+WYMeditor.editor.prototype.wymplugin = function() {
+    var wym = this;
+
+    function onkeyup(evt) {
+        alert("you pressed a key");
+    }
+
+    //add a handler to the keyup event
+    jQuery(wym._doc).bind("keyup", onkeyup);
+
+    //example code
+    jQuery(this._box).find(this._options.toolSelector).hover(
+        function() { wym.status('hover'); },
+        function() { wym.status('hi'); }
+    );
+
+};
+
+
+
