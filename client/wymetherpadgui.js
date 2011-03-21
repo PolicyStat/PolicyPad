7294fdda465807064bf99d31e1fec9e5403794db
Jimmy Theis <jimmy@jetheis.com>
2011-03-21 14:24:54 -0400

    Change submission event to be recurring
    Signed-off-by: Jimmy Theis <jimmy@jetheis.com>

--- client/wymetherpadgui.js
+++ client/wymetherpadgui.js
@@ -54,7 +54,8 @@ function WymEtherpadGUI(guioptions, options, wym) {
 
   // Automatically send changes
   // WymEtherpad should check to see if there are actually changes
-  setTimeout(function() {
+  var 
+  setInterval(function() {
      handleUIEvent("timer tick");
   }, 250);
 
