e7629b2874885c88b9d980573d9faca9bf093a79
Jimmy Theis <jimmy@jetheis.com>
2011-04-04 10:24:15 -0400

    Fix anonymous function signature
    Signed-off-by: Jimmy Theis <jimmy@jetheis.com>

--- client/policypad.js
+++ client/policypad.js
@@ -284,7 +284,7 @@ function optimizeChangeset(oldText, changeset) {
 
   do {
     origChangeset = changeset;
-    $.each(optimizers, function(optimizer) {
+    jQuery.each(optimizers, function(i, optimizer) {
       changeset = optimizer(changeset);
     });
   } while (changeset != origChangeset);
