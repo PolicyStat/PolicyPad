21746b812a22466077254136c881c52ed97bb966
Kevin Wells <wellska1@rose-hulman.edu>
2011-04-25 14:05:35 -0400

    Remove unneeded debugging alerts
    Signed-off-by: Kevin Wells <wellska1@rose-hulman.edu>

--- client/etherpaddocument.js
+++ client/etherpaddocument.js
@@ -43,11 +43,7 @@ function EtherpadDocument(funcHtml, funcXhtml, initialText) {
   //to manipulate both the text and the text/apool mappings.
   this._funcHtml(initialText.text);
   this._prevHtml = this._funcHtml();
-  if (this._prevHtml == null)
-    alert("Previous Html is null");
   this._prevXhtml = initialText.text;//this._funcXhtml();
-  if (this._prevXhtml == null)
-    alert("Previous XHtml is null");
   this._pendingChangeset = null;
 }
 
@@ -65,8 +61,6 @@ EtherpadDocument.prototype.applyChangeset = function(changeset, apool) {
     var newXhtml = applyChangeset(this._prevXhtml, changeset);
     this._funcHtml(newXhtml);
     this._prevHtml = this._funcHtml();
-  if (this._prevHtml == null)
-    alert("Previous Html is null");
     this._prevXhtml = newXhtml;
   }
   else {
@@ -84,12 +78,8 @@ EtherpadDocument.prototype.applyChangeset = function(changeset, apool) {
     //be able to grab both selections
     var oldXhtml = this._prevXhtml;
     this._prevXhtml = applyChangeset(this._prevXhtml, changeset);
-  if (this._prevXhtml == null)
-    alert("Previous XHtml is null");
     this._funcHtml(this._prevXhtml);
     this._prevHtml = this._funcHtml();
-  if (this._prevHtml == null)
-    alert("Previous Html is null");
 
     //4. Apply merged to current HTML
     var newXhtml = applyChangeset(oldXhtml, merged);
@@ -106,8 +96,6 @@ EtherpadDocument.prototype.hasPendingChangeset = function() {
 EtherpadDocument.prototype.changesetAccepted = function() {
   this._prevHtml = this._pendingHtml;
   this._prevXhtml = applyChangeset(this._prevXhtml, this._pendingChangeset);
-  if (this._prevXhtml == null)
-    alert("Previous XHtml is null");
   this._pendingChangeset = null;
 }
 
